1dfe21298f9f340ec0fd8cd41092e0a25348252f
piethrower <piethrowerchamp@gmail.com>
2021-10-05 13:21:34 -0400

    Added test file

---
+++ tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import unittest
+
+import Dice_Roller as dice
+
+class TestDiceRoller(unittest.TestCase):
+
+    def test_func(self):
+        self.assertEqual(dice.parseFunc("abs","-10"), 10)
+        self.assertEqual(dice.parseFunc("abs","0"), 0)
+        self.assertEqual(dice.parseFunc("abs","10"), 10)
+
+if __name__ == '__main__':
+    # Main module
+    unittest.main(buffer=True)
