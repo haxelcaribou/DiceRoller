024f00781a1e30bf8f485b48920188c3a9b23f35
piethrower <piethrowerchamp@gmail.com>
2021-10-24 20:24:47 -0400

    Added tests for min and max

--- tests.py
+++ tests.py
@@ -11,6 +11,14 @@ class TestDiceRoller(unittest.TestCase):
         self.assertEqual(dice.parseFunc("abs","0"), 0)
         self.assertEqual(dice.parseFunc("abs","10"), 10)
 
+        self.assertEqual(dice.parseFunc("min","-10, 0, 10"), -10)
+        self.assertEqual(dice.parseFunc("min","10"), 10)
+        self.assertEqual(dice.parseFunc("min","10, 0"), 0)
+
+        self.assertEqual(dice.parseFunc("max","-10, 0, 10"), 10)
+        self.assertEqual(dice.parseFunc("max","10"), 10)
+        self.assertEqual(dice.parseFunc("max","10, 0"), 10)
+
 if __name__ == '__main__':
     # Main module
     unittest.main(buffer=True)
