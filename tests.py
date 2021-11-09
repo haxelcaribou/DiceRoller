bf3396c5dde491fd96490eb0475566f3eec8f612
piethrower <piethrowerchamp@gmail.com>
2021-11-09 12:28:54 -0500

    added even more function tests

--- tests.py
+++ tests.py
@@ -56,6 +56,38 @@ class TestDiceRoller(unittest.TestCase):
         self.assertEqual(dice.parseFunc("pow", "10,0"), 1)
         self.assertEqual(dice.parseFunc("pow", "2,-2"), 0.25)
 
+        self.assertEqual(dice.parseFunc("round", "pi"), 3)
+        self.assertEqual(dice.parseFunc("round", "0"), 0)
+        self.assertEqual(dice.parseFunc("round", "1.2"), 1)
+        self.assertEqual(dice.parseFunc("round", "1.8"), 2)
+        self.assertEqual(dice.parseFunc("round", "-10.8"), -11)
+        self.assertEqual(dice.parseFunc("round", "-tau"), -6)
+
+        self.assertEqual(dice.parseFunc("floor", "pi"), 3)
+        self.assertEqual(dice.parseFunc("floor", "0"), 0)
+        self.assertEqual(dice.parseFunc("floor", "1.2"), 1)
+        self.assertEqual(dice.parseFunc("floor", "1.8"), 1)
+        self.assertEqual(dice.parseFunc("floor", "-10.8"), -11)
+        self.assertEqual(dice.parseFunc("floor", "-tau"), -7)
+
+        self.assertEqual(dice.parseFunc("ceil", "pi"), 4)
+        self.assertEqual(dice.parseFunc("ceiling", "pi"), 4)
+        self.assertEqual(dice.parseFunc("ceil", "0"), 0)
+        self.assertEqual(dice.parseFunc("ceil", "1.2"), 2)
+        self.assertEqual(dice.parseFunc("ceil", "1.8"), 2)
+        self.assertEqual(dice.parseFunc("ceil", "-10.8"), -10)
+        self.assertEqual(dice.parseFunc("ceil", "-tau"), -6)
+
+        self.assertEqual(dice.parseFunc("log", "2,2"), 1)
+        self.assertEqual(dice.parseFunc("log", "8,2"), 3)
+        self.assertEqual(dice.parseFunc("log", "10,3"), math.log(10,3))
+        self.assertEqual(dice.parseFunc("log", "2"), math.log(2))
+        self.assertEqual(dice.parseFunc("log", "10"), math.log(10))
+
+        self.assertEqual(dice.parseFunc("avg", "-10, 0, 10"), 0)
+        self.assertEqual(dice.parseFunc("avg", "10"), 10)
+        self.assertEqual(dice.parseFunc("avg", "10, 0"), 5)
+
 
 if __name__ == '__main__':
     # Main module
