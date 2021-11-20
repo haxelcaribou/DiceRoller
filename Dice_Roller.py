cf3521a039d6d019e694bbd68d15e5216c121c55
piethrower <piethrowerchamp@gmail.com>
2021-11-19 20:07:04 -0500

    pylint

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''command line dice roller'''
 
 import random
 import re
@@ -16,7 +17,6 @@ from os import system, name
 # commas in large numbers
 # >>> testing <<<
 
-
 # import the readline module for arrow functionality if it exists
 try:
     import readline
@@ -25,8 +25,11 @@ except ImportError:
     pass
 
 
-# define ANSI colors
+__author__ = "Pie Thrower"
+
+
 class ANSI:
+    '''define ANSI colors'''
     RED = '\033[91m'
     BLUE = '\033[94m'
     GREEN = '\033[92m'
@@ -45,95 +48,97 @@ class ANSI:
 ans = 0
 
 # compile regexes
-diceRegex = re.compile(r"^(\d+d\d+((t|b)\d+)?(?=( |$)))+")
-intRegex = re.compile(r"^-? ?\d+$")
-floatRegex = re.compile(r"^-? ?\d*\.\d+$")
-multRegex = re.compile(r"([\*/%])")
-addRegex = re.compile(r"(\+|(?<=\w) ?-)")
-mooRegex = re.compile(r"^mo{2,}$")
-funcRegex = re.compile(r"\w+$")
-topRegex = re.compile(r"(?<=t)\d+$")
-bottomRegex = re.compile(r"(?<=b)\d+$")
-diceNumberRegex = re.compile(r"^\d+(?=d)")
-diceSidesRegex = re.compile(r"(?<=d)\d+")
+DICE_REGEX = re.compile(r"^(\d+d\d+((t|b)\d+)?(?=( |$)))+")
+INT_REGEX = re.compile(r"^-? ?\d+$")
+FLOAT_REGEX = re.compile(r"^-? ?\d*\.\d+$")
+MULT_REGEX = re.compile(r"([\*/%])")
+ADD_REGEX = re.compile(r"(\+|(?<=\w) ?-)")
+MOO_REGEX = re.compile(r"^mo{2,}$")
+FUNC_REGEX = re.compile(r"\w+$")
+TOP_REGEX = re.compile(r"(?<=t)\d+$")
+BOTTOM_REGEX = re.compile(r"(?<=b)\d+$")
+DICE_NUMBER_REGEX = re.compile(r"^\d+(?=d)")
+DICE_SIDES_REGEX = re.compile(r"(?<=d)\d+")
 
 
 # help text
-helpText = ""
+HELP_TEXT = ""
 with open("InfoText.txt", "r") as filehandle:
-    helpText = filehandle.read()
+    HELP_TEXT = filehandle.read()
 
 
-def parseString(input):
-    input = input.strip()
+def parse_string(input_string):
+    '''take input string and decide where to send it'''
+    input_string = input_string.strip()
 
     # convert numbers to integers
-    if intRegex.match(input):
-        return int(input.replace(" ", ""))
+    if INT_REGEX.match(input_string):
+        return int(input_string.replace(" ", ""))
     # also convert decimals
-    if floatRegex.match(input):
-        return float(input.replace(" ", ""))
+    if FLOAT_REGEX.match(input_string):
+        return float(input_string.replace(" ", ""))
 
     # do math if there are any math symbols
-    mathStrings = ("+", "-", "*", "/", "%", "^")
-    for sub in mathStrings:
-        if sub in input:
-            return parseMath(input)
+    math_strings = ("+", "-", "*", "/", "%", "^")
+    for sub in math_strings:
+        if sub in input_string:
+            return parse_math(input_string)
 
     # if it's valid dice notation roll it
-    if diceRegex.match(input):
-        return rollDice(input)
+    if DICE_REGEX.match(input_string):
+        return roll_dice(input_string)
 
     # if either side of a math expression is empty replace it with zero
-    if input == "":
+    if input_string == "":
         return 0
 
     # gives the last valid answer
-    if input == "ans":
+    if input_string == "ans":
         return ans
 
     # booleans
-    if input == "true":
+    if input_string == "true":
         return 1
 
-    if input == "false":
+    if input_string == "false":
         return 0
 
     # constants
-    if input == "pi":
+    if input_string == "pi":
         return math.pi
-    if input == "tau":
+    if input_string == "tau":
         return math.tau
-    if input == "euler":
+    if input_string == "euler":
         return math.e
-    if input == "nan":
+    if input_string == "nan":
         return math.nan
-    if input == "inf":
+    if input_string == "inf":
         return math.inf
 
     # handle shortcuts
-    if input == "t":
-        return rollDie("1d20")
-    if input == "a":
-        return rollDie("2d20b1")
-    if input == "d":
-        return rollDie("2d20t1")
-    if input == "s":
-        return rollDie("4d6b1")
-    if input == "s6":
-        return parseString("4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1")
-    if input == "s8":
-        return parseString("4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1")
+    if input_string == "t":
+        return roll_die("1d20")
+    if input_string == "a":
+        return roll_die("2d20b1")
+    if input_string == "d":
+        return roll_die("2d20t1")
+    if input_string == "s":
+        return roll_die("4d6b1")
+    if input_string == "s6":
+        return parse_string("4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1")
+    if input_string == "s8":
+        return parse_string("4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1 + 4d6b1")
 
     raise ValueError("Invalid Input")
 
 
-def parseFunc(function, input):
-    args = [parseString(i) for i in input.split(",")]
-    argNum = len(args)
+def parse_func(function, input_string):
+    '''apply functions to the result of the input'''
+    args = [parse_string(i) for i in input_string.split(",")]
+    arg_num = len(args)
 
     if function == "abs":
-        if argNum > 1:
+        if arg_num > 1:
             raise ValueError("Abs function takes only one argument")
         result = abs(args[0])
 
@@ -144,100 +149,100 @@ def parseFunc(function, input):
         result = max(args)
 
     elif function == "sqrt":
-        if argNum > 1:
+        if arg_num > 1:
             raise ValueError("Sqrt function takes only one argument")
         result = math.sqrt(args[0])
 
-    elif function == "degrees" or function == "deg":
-        if argNum > 1:
+    elif function in ("degrees", "deg"):
+        if arg_num > 1:
             raise ValueError("Degrees function takes only one argument")
         result = math.degrees(args[0])
 
-    elif function == "radians" or function == "rad":
-        if argNum > 1:
+    elif function in ("radians", "rad"):
+        if arg_num > 1:
             raise ValueError("Radians function takes only one argument")
         result = math.radians(args[0])
 
     elif function == "sin":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Sin function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.sin(math.radians(args[0]))
         else:
             result = math.sin(args[0])
 
     elif function == "cos":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Cos function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.cos(math.radians(args[0]))
         else:
             result = math.cos(args[0])
 
     elif function == "tan":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Tan function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.tan(math.radians(args[0]))
         else:
             result = math.tan(args[0])
 
     elif function == "atan2":
-        if argNum > 3 or argNum < 2:
+        if arg_num > 3 or arg_num < 2:
             raise ValueError("atan function takes two or three arguments")
-        if argNum == 3 and args[2] == True:
+        if arg_num == 3 and bool(args[2]):
             result = math.degrees(math.atan2(args[0], args[1]))
         else:
             result = math.atan2(args[0], args[1])
 
     elif function == "asin":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Asin function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.degrees(math.asin(args[0]))
         else:
             result = math.asin(args[0])
 
     elif function == "acos":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Acos function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.degrees(math.acos(args[0]))
         else:
             result = math.acos(args[0])
 
     elif function == "atan":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Atan function takes one or two arguments")
-        if argNum == 2 and args[1] == True:
+        if arg_num == 2 and bool(args[1]):
             result = math.degrees(math.atan(args[0]))
         else:
             result = math.atan(args[0])
 
     elif function == "pow":
-        if argNum != 2:
+        if arg_num != 2:
             raise ValueError("Pow function takes exactly two arguments")
         result = pow(args[0], args[1])
 
     elif function == "round":
-        if argNum > 1:
+        if arg_num > 1:
             raise ValueError("Round function takes only one argument")
         result = round(args[0])
 
     elif function == "floor":
-        if argNum > 1:
+        if arg_num > 1:
             raise ValueError("Floor function takes only one argument")
         result = math.floor(args[0])
 
-    elif function == "ceil" or function == "ceiling":
-        if argNum > 1:
+    elif function in ("ceil", "ceiling"):
+        if arg_num > 1:
             raise ValueError("Ceil function takes only one argument")
         result = math.ceil(args[0])
 
     elif function == "log":
-        if argNum > 2:
+        if arg_num > 2:
             raise ValueError("Log function takes one or two arguments")
-        if argNum == 2:
+        if arg_num == 2:
             result = math.log(args[0], args[1])
         else:
             result = math.log(args[0])
@@ -253,45 +258,46 @@ def parseFunc(function, input):
     return result
 
 
-def parseParens(input):
+def parse_parens(input_string):
     parens = []
     i = 0
-    while i < len(input):
-        c = input[i]
+    while i < len(input_string):
+        c = input_string[i]
         if c == "(":
             parens.append(i)
         elif c == ")":
             begin = parens.pop()
             end = i
-            before = input[:begin].strip()
-            inner = input[begin + 1:end].strip()
-            after = input[end + 1:].strip()
+            before = input_string[:begin].strip()
+            inner = input_string[begin + 1:end].strip()
+            after = input_string[end + 1:].strip()
 
-            function_match = funcRegex.search(before)
+            function_match = FUNC_REGEX.search(before)
             if function_match:
                 function = function_match.group()
-                inner = parseFunc(function, inner)
+                inner = parse_func(function, inner)
                 before = before[:-len(function)]
             else:
-                inner = parseString(inner)
+                inner = parse_string(inner)
 
-            input = before + str(inner) + after
+            input_string = before + str(inner) + after
             i = begin
         i += 1
 
     if len(parens) == 0:
-        return parseString(input)
+        return parse_string(input_string)
     raise ValueError("Unmatched Parenthesis")
 
 
-def parseMath(input):
-    if addRegex.search(input):
-        parts = addRegex.split(input)
-        sum = parseString(parts[0])
+def parse_math(input_string):
+    '''Handle basic math'''
+    if ADD_REGEX.search(input_string):
+        parts = ADD_REGEX.split(input_string)
+        sum = parse_string(parts[0])
         output_string = "{:g}".format(sum)
         i = 1
         while i < len(parts):
-            term = parseString(parts[i + 1])
+            term = parse_string(parts[i + 1])
             if parts[i] == "+":
                 sum += term
                 output_string += " + "
@@ -303,13 +309,13 @@ def parseMath(input):
         print("{} = {:g}\n".format(output_string, sum))
         return sum
 
-    if "*" in input or "/" in input or "%" in input:
-        parts = multRegex.split(input)
-        product = parseString(parts[0])
+    if "*" in input_string or "/" in input_string or "%" in input_string:
+        parts = MULT_REGEX.split(input_string)
+        product = parse_string(parts[0])
         output_string = "{:g}".format(product)
         i = 1
         while i < len(parts):
-            term = parseString(parts[i + 1])
+            term = parse_string(parts[i + 1])
             if parts[i] == "*":
                 product *= term
                 output_string += " * "
@@ -324,41 +330,42 @@ def parseMath(input):
         print("{} = {:g}\n".format(output_string, product))
         return product
 
-    if "^" in input:
-        parts = input.split("^", 1)
-        base = parseString(parts[0])
-        exponent = parseString(parts[1])
+    if "^" in input_string:
+        parts = input_string.split("^", 1)
+        base = parse_string(parts[0])
+        exponent = parse_string(parts[1])
         power = base ** exponent
         print("{:g} ^ {:g} = {:g}\n".format(base, exponent, power))
         return power
 
-    if "-" in input:
-        return -parseString(input[1:])
+    if "-" in input_string:
+        return -parse_string(input_string[1:])
 
     raise ValueError("Invalid Input")
 
 
-def rollDie(input):
-    print(ANSI.GREEN + input + ANSI.END)
+def roll_die(input_string):
+    '''roll a single type of die'''
+    print(ANSI.GREEN + input_string + ANSI.END)
 
-    numDice = int(diceNumberRegex.search(input).group())
-    diceSides = int(diceSidesRegex.search(input).group())
-    if numDice == 0 or diceSides == 0:
+    num_dice = int(DICE_NUMBER_REGEX.search(input_string).group())
+    dice_sides = int(DICE_SIDES_REGEX.search(input_string).group())
+    if num_dice == 0 or dice_sides == 0:
         return 0
 
-    tSearch = topRegex.search(input)
-    bSearch = bottomRegex.search(input)
-    if tSearch:
-        return removeDice(numDice, diceSides, int(tSearch.group()), True)
-    elif bSearch:
-        return removeDice(numDice, diceSides, int(bSearch.group()), False)
+    top_search = TOP_REGEX.search(input_string)
+    bottom_search = BOTTOM_REGEX.search(input_string)
+    if top_search:
+        return remove_dice(num_dice, dice_sides, int(top_search.group()), True)
+    if bottom_search:
+        return remove_dice(num_dice, dice_sides, int(bottom_search.group()), False)
 
     sum = 0
 
-    # roll numDice number of times
-    for i in range(numDice):
-        # get random number in range diceSides
-        roll = random.randint(1, diceSides)
+    # roll num_dice number of times
+    for i in range(num_dice):
+        # get random number in range dice_sides
+        roll = random.randint(1, dice_sides)
         print(roll, end=" ", flush=True)
         sum += roll
 
@@ -367,7 +374,8 @@ def rollDie(input):
     return sum
 
 
-def removeDice(num, dice, remove, top):
+def remove_dice(num, dice, remove, top):
+    '''roll a number of dice and remove from the top or bottom'''
     rolls = []
 
     # roll num number of times
@@ -379,18 +387,18 @@ def removeDice(num, dice, remove, top):
                 "removed": False}
         rolls.append(info)
 
-    sortedRolls = sorted(rolls, key=lambda item: item.get("roll"))
+    sorted_rolls = sorted(rolls, key=lambda item: item.get("roll"))
 
     if remove > num:
         raise ValueError("More dice removed than rolled")
 
     # print and sum
-    if top:
+    if bool(top):
         for i in range(remove):
-            rolls[sortedRolls[len(rolls) - i - 1]["order"]]["removed"] = True
+            rolls[sorted_rolls[len(rolls) - i - 1]["order"]]["removed"] = True
     else:
         for i in range(remove):
-            rolls[sortedRolls[i]["order"]]["removed"] = True
+            rolls[sorted_rolls[i]["order"]]["removed"] = True
 
     sum = 0
 
@@ -408,18 +416,20 @@ def removeDice(num, dice, remove, top):
     return sum
 
 
-def rollDice(input):
-    dice = input.split(" ")
+def roll_dice(input_string):
+    '''roll multiple types of dice and add the results'''
+    dice = input_string.split(" ")
 
     total = 0
 
     for die in dice:
-        total += rollDie(die.strip())
+        total += roll_die(die.strip())
 
     return total
 
 
-def clearScreen():
+def clear_screen():
+    '''clear the terminal screen'''
     if name == 'nt':
         system('cls')
     elif name == "posix":
@@ -428,30 +438,31 @@ def clearScreen():
         print(ANSI.CLEAR, end="")
 
 
-
 def run():
+    '''main program'''
     # clear screen at program start
-    clearScreen()
+    clear_screen()
 
     # keep taking commands
     while 1:
         # take input
-        i = input(ANSI.BLUE + "Enter Value: " + ANSI.END).strip().lower()
+        i = input_string(ANSI.BLUE + "Enter Value: " +
+                         ANSI.END).strip().lower()
 
         # help
-        if i == "help" or i == "info":
+        if i in ("help", "info"):
             print()
-            print(helpText)
+            print(HELP_TEXT)
 
         # quit program
-        elif i == "exit" or i == "end" or i == "quit" or i == "q":
-            clearScreen()
+        elif i in ("exit", "end", "quit", "q"):
+            clear_screen()
             break
 
         elif i == "clear":
-            clearScreen()
+            clear_screen()
 
-        elif mooRegex.match(i):
+        elif MOO_REGEX.match(i):
             print("\nMoo")
 
         elif i == "":
@@ -459,12 +470,13 @@ def run():
 
         else:
             try:
-                ans = parseParens(i)
+                ans = parse_parens(i)
                 print(ANSI.BOLD, "total = {:g}".format(ans), ANSI.END, sep="")
             except ValueError as e:
                 print(e)
             except ZeroDivisionError:
                 print("Divide by Zero")
+
 
 if __name__ == '__main__':
     run()
